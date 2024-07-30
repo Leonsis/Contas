0c6f3f8477a0e8048cd09fea9d4f3ca9ab2a0834
Caio Leonni <127361640+Leonsis@users.noreply.github.com>
2024-07-30 16:56:50 -0300

    Add files via upload

---
+++ src/JS/avisos_Alertas.js
@@ -0,0 +1,24 @@
+const obs = document.getElementById('obs');
+const boxNome = document.getElementById('boxNome');
+const boxValorMeta= document.getElementById('boxValorMeta')
+function alerta2() {
+    obs.classList.remove('d-none');
+    boxNome.classList.add('d-none');
+    boxValorMeta.classList.add('d-none');
+}
+
+function removerAlerta() {
+    obs.classList.add('d-none');
+    boxNome.classList.remove('d-none');
+    boxValorMeta.classList.remove('d-none');
+}
+
+const aviso = document.getElementById('aviso')
+function alerta() {
+    aviso.classList.remove(
+'d-none');
+}
+
+function remuveAlerta() {
+    aviso.classList.add('d-none');
+}
